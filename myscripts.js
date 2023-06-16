be10134120281c7405c2426b3be40c2313142c7d
Brizly <87358177+Bbrizly@users.noreply.github.com>
2023-06-16 19:12:31 +0300

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,10 @@
+var button = document.querySelector('button');
+var box = document.getElementById('changeMe');
+a = true
+
+function changeColor(){
+    if(box.style.background == 'red'){
+        box.style.background = 'blue'
+    }else{
+        box.style.background = 'red'
+    }}
