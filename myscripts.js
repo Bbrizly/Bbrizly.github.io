7c25768d29f7b5a3bafd2065324a066392f48b2f
Brizly <87358177+Bbrizly@users.noreply.github.com>
2025-01-24 18:41:15 -0400

    Full Re-vamp

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,18 @@
-var button = document.querySelector('button');
-var box = document.getElementById('changeMe');
-a = true
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('button');
+    button.addEventListener('click', () => {
+        document.body.style.background = getRandomGradient();
+    });
 
-function changeColor(){
-    if(box.style.background == 'red'){
-        box.style.background = 'blue'
-    }else{
-        box.style.background = 'red'
-    }}
+    function getRandomGradient() {
+        const colors = [
+            `rgb(${random255()}, ${random255()}, ${random255()})`,
+            `rgb(${random255()}, ${random255()}, ${random255()})`
+        ];
+        return `linear-gradient(90deg, ${colors[0]}, ${colors[1]})`;
+    }
+
+    function random255() {
+        return Math.floor(Math.random() * 256);
+    }
+});
